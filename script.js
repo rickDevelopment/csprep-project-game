89b257ce2f67bc201405d01e4b3d502c1bd567f6
Rick <alrick.chalar@gmail.com>
2019-05-12 22:23:02 -0400

    add refactor code

--- script.js
+++ script.js
@@ -1,27 +1,44 @@
 
 const submitButton = document.querySelector('.submit-button');
 const answer = document.querySelector('#answer');
-const codeBlock = document.querySelector('.code-block');
-
-submitButton.onclick= handleSubmit;
+const questionArea = document.querySelector('.question-area');
 
 //Function will run when the submit button is clicked
-function handleSubmit(){
+function handleSubmitAnswer(){
  //convert user answer to  integer 
-  userAnswer  = parseInt(answer.value)
+  
+ checkAnswer()
+}
+
+function loadQuestions(array){
+
+  for(let i = 0; i < array.length; i++){
+    questionArea.textContent = array[i]["question"]
+  }
+
+}
+
+function displayQuestion(questions){
+  question.textContent = questions
+}
+
+function checkAnswer(){
+ let userAnswer  = parseInt(answer.value)
   console.log(typeof userAnswer)
-  solution = gameArray[0].solution 
-  if(userAnswer === solution){
+ let quizAnswer = gameArray[0].answer 
+
+  if(userAnswer === quizAnswer){
     alert('Correct')
   }else{
     alert('Incorrect')
   }
-  alert(`The Correct Answer is: ${gameArray[0].solution} answer submitted is ${answer.value}` )
+  alert(`The Correct Answer is: ${gameArray[0].answer} answer submitted is ${answer.value}` ) 
 }
 
+/*Array of object for questions */
 const gameArray = [
 
-  {"code-block": `
+  {"question": `
     var myFun2 = function() { 
       var num = -1
       function incrementUntil(max) { 
@@ -34,12 +51,30 @@ const gameArray = [
     }    
     myFun2() }
     `,
-    solution:3
-  }
+    answer:3
+  },
+  {'question': `this is some code 1`,
+    answer: false
+},
+
+  {'question': `this is some code 2`,
+    answer: 10
+},
+
+  {'question': `this is some code 3`,
+    answer: 25
+},
+
+  {'question': `this is some code 4`,
+    answer: true
+}
+
+
   ]
 
-
- codeBlock.textContent = gameArray[0]["code-block"]
+loadQuestions(gameArray)
+submitButton.onclick= handleSubmitAnswer;
+ 
 
  
 
